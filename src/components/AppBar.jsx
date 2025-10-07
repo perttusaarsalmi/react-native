55b04ef8a11e549772d091a80edf4b8ae0f58244
Perttu Saarsalmi <ext.perttu.saarsalmi@metsagroup.com>
2025-10-07 12:46:23 +0300

    osa10 react native step6 and 7

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -2,24 +2,44 @@ import { View, StyleSheet, Pressable } from 'react-native';
 import Constants from 'expo-constants';
 import theme from '../theme';
 import Text from './Text';
+import {ScrollView} from 'react-native';
+
+import { useNavigate } from 'react-router-native';
 
 const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
     backgroundColor: theme.colors.appBar, // black
-    height: 56 + Constants.statusBarHeight, // typical app bar height
-    justifyContent: 'center',
+    height: 56 + Constants.statusBarHeight,
     paddingHorizontal: 16,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  pressable: {
+    paddingBottom: 8,
+   paddingRight: 15
   },
 });
 
 const AppBar = () => {
+  const navigate = useNavigate();
+
   return (
-    <Pressable style={styles.container} onPress={() => {}}>
-      <Text color="appBarText" fontWeight="bold" fontSize="heading">
-        Repositories
-      </Text>
-    </Pressable>
+    <View style={styles.container}>
+        <ScrollView horizontal>
+      <Pressable style={styles.pressable} onPress={() => navigate('/')}>
+        <Text color="appBarText" fontWeight="bold" fontSize="heading">
+          Repositories
+        </Text>
+      </Pressable>
+      <Pressable style={styles.pressable} onPress={() => navigate('/signIn')}>
+        <Text color="appBarText" fontWeight="bold" fontSize="heading">
+          Sign in
+        </Text>
+      </Pressable>
+      </ScrollView>
+    </View>
   );
 };
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,9 @@ import Constants from 'expo-constants';
 import { Text, StyleSheet, View } from 'react-native';
 import RepositoryList from './RepositoryList';
 import AppBar from './AppBar'
+import SignIn from './SignIn'
+import { Route, Routes, Navigate } from 'react-router-native';
+
 
 const styles = StyleSheet.create({
   container: {
@@ -14,7 +17,11 @@ const Main = () => {
   return (
     <View style={styles.container}>
         <AppBar />
-        <RepositoryList />
+           <Routes>
+                <Route path="/" element={<RepositoryList />} />
+                <Route path="/signIn" element={<SignIn />} />
+                <Route path="*" element={<Navigate to="/" replace />} />
+           </Routes>
     </View>
   );
 };
